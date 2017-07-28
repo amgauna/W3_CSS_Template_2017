67df5b08f63188584ced108449f613dabda842ba
Ana Mercedes Gauna <amgauna@gmail.com>
2017-07-28 11:47:36 -0300

    Add files via upload

---
+++ curriculo.php
@@ -0,0 +1,165 @@
+
+<?php require('cabecalho.php'); ?>
+
+<body>
+
+          <center>
+          <div class="section" style="background:darkblue; margin:0; padding:0px; border:1px solid #aaa">
+               <img src="http://www.amgauna.eti.br/img/imagem-amgauna-etibr.jpg" style="height:50%; width:100%; align:center">
+          </div>
+          </center>
+
+
+        <!-- inicio do MENU CURRICULO/ Bootstrap / Curriculo -->
+        <div class="section" style="background:silver; margin:0; padding:0px; border:1px solid #aaa">
+        <nav role="navigation" class="navbar navbar-default navbar-static-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                     <a class="navbar-brand" href="#"> <?php include('contadorvisitas.php'); ?> </a>
+                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">  
+                             <span class="sr-only">Toggle navigation</span>
+                             <span class="icon-bar"></span>    <!--  1 -->
+                             <span class="icon-bar"></span>    <!--  2  -->
+                             <span class="icon-bar"></span>    <!--  3 -->
+                             <span class="icon-bar"></span>    <!--  4 -->
+                             <span class="icon-bar"></span>    <!--  5 -->
+                             <span class="icon-bar"></span>    <!--  6 -->
+                             <span class="icon-bar"></span>    <!--  7 -->
+                             <span class="icon-bar"></span>    <!--  8 -->
+                             <span class="icon-bar"></span>    <!--  9 -->
+                             <span class="icon-bar"></span>    <!--  10 -->
+                     </button>
+                 </div>
+                 <div class="collapse navbar-collapse" id="navbar-collapse">   
+                      <ul class="nav navbar-nav navbar-right">
+                          <li> <a href="home.php">Home</a> </li>
+                          <li class="active"> <a href="curriculo.php">Curriculo</a> </li>
+                          <li> <a href="servicos.php">Serviços</a> </li>
+                          <li> <a href="artigos.php">Artigos</a> </li>
+                          <li> <a href="instalacao.php">Instalação</a> </li>
+                          <li> <a href="antivirus.php">Antivirus</a> </li>
+                          <li> <a href="http://pt.gravatar.com/amgauna">Sobre Mim</a> </li>
+                          <li> <a href="http://anagauna.wordpress.com">Wodpress</a> </li>
+                          <li> <a href="http://anagaunatech.blogspot.com">Blogspot</a> </li>
+                          <li> <a href="https://plus.google.com/+AnaGaunaTech">Google++</a> </li>
+                      </ul> 
+                 </div>
+            </div>
+        </nav>
+        </div>
+        <!-- fim do MENU -->
+
+
+    <body>	
+        <div class="section" style="background:lightrose; margin:0; padding:6px; border:1px solid #aaa"> 	<!-- #F0FFF0  cor manÃ¡ -->				  
+            <div class="container" align="justify"> 
+				 <div class="row">			    		
+					  <div class="col-md-10">   <!-- inicio da primeira coluna -->
+					        <p><br><p>
+                            <p><br></p>
+							<h3><b>ANA MERCEDES GAUNA</b></h3> 
+						   <p><br></p>
+                           <img src="http://www.amgauna.eti.br/img/AnaGauna-2014B.jpg" height="200" width="200" class="img-responsive" align="center">
+						    <p><br></p>
+                           <h3><b><u>SOBRE MIM:</u></b></h3>						 
+                           <p><br></p>
+					            <ul> <h4>
+								     <li>Profissão: Analista de Sistemas, Negócios, Projetos e Infraestrutura Senior #CTO #MCSE #CCNA2</li> <p>
+								     <li>CRA-RJ: 03-03161 Tecnólogo | ABRAWEB: 66132 Webmaster | ABRACEM: Q27795 Consultora de Tecnologia; </li> </p>
+									 <li>Microsoft Partner MPN: 4970237 | Microsoft Cloud solution Provider (CSP) </li> <p>
+									 <li>MCSE: 098-02070 | MCSE Infraestrutura Microsoft Windows Server + TCP/IP + Microsoft SQL Server</li> 
+									 <li>Suporte Técnico com foco em buscar soluções para problemas técnico-administrativos.
+									 <p> <p>
+									 <li>Formação Técnica: MCSE Infraestrutura Servidor (Microsoft Windows Server + TCP/IP) / Concluido: 2000); </li> <p>
+									 <li>Formação Técnica: MCDBA (Administração do Microsoft SQL Server / Concluido: 2000); </li> <p>
+									 <li>Formação Técnica: Cisco Networking Academy CCNA1 + CCNA Routing and Switching (Concluido em 2008); </li> <p>
+									 <li>Formação Superior: Tecnológia em Processamento de Dados (FACHA - Faculdades Integradas Helio Alonso / Concluido: 2003) </li> <p>
+									 <li>Formação Técnica: Programação de Computadores (IBPI - Instituto Brasileiro de Informatica / Concluido em 1998) </li> 
+									     <p> <p>
+									 <li>Sou Brasileira, Carioca, solteira; estudei inglês e francês na década de 80, mas não sou fluente em inglês e francês.
+ 									     Leio livros técnicos em inglês da Área de Administração e Ciência da Computação desde a década de 80. 
+										 Sempre morei e trabalhei no Rio de Janeiro/RJ (Brazil). </li> 
+										 <p> <p>
+									 <li>Possuo 27 anos de experiência profissional com carteira assinada (CLT), sendo os ultimos 20 anos com experiência 
+									     em cargo de gestão administrativa. Estou procurando um emprego novo. Estou desempregada ha mais de 24 meses 
+										 (pedi demissão em novembro/2014, pensei muito e decidi sair, quando discordei de decição tomada pela Diretoria).
+										 Estou aceitando receber trabalhos na Zona Sul do Rio de Janeiro, como autônoma (profissional liberal).</li> 
+										 <p> <p>
+									 <li>Desde 2014/2015/2016/2017 estou estudando textos de artigos tÃ©cnicos no Facebook, Linkedin e Twitter, 
+									     relativos aos assuntos: Big Data, Data Analytics, Data Gov, Data Science, Iot, Inteligência Artificial, 
+										 Robótica, Privacidade Digital, Direito Cibernético, CiberSecurity, Bitcoin, Blockchain, Fintech, etc. </li> 
+										 <p> <p>
+									 <li>Sou AUTODIDATA, em 2015/2016 comprei livros novos que estou lendo aos poucos, de assuntos diversos: 
+									     Cobit5, ITIL3, Auditoria de Sistemas, Segurança das Informações, Qualidade, Marketing de Conteúdo, etc. </li> 
+										 <p> <p>
+									 <li>Em 2015/2016/2017 eu decidi voltar a estudar as linguagens de programação de computadores C e C++ (novamente), e 
+										 então comprei diversos livros novos: HTML5, CSS3, PHP, JavaScript, Java, Python, etc, que leio aos poucos. </li> 
+										 <p> <p>
+									 <li>Sou Webmaster, tenho registro de dominio próprio na internet desde 1998 (Cadastro: FAPESP/SP|Registro.BR|CGI.BR),
+                                         meu primeiro site foi hospedado no Geocities (Yahoo) em 1997. Meu velho email amgauna@geocities.com ainda funciona.									 </li> 
+									     <p> <p>
+									 <li>Em 2016 fiz um cadastro na <b>Associação Brasileira de Normas Técnicas</b> (http://www.abnt.org.br); 
+									     comprei a coleção completa das normas de segurança relativas a Tecnologia da Informação (Normas ABNT IEC ISO 27001/27002/27003), 
+										 as normas de Gestão de Projetos, e um livro maravilhoso de Gestão da Qualidade, que estou lendo aos poucos. </li> <p>							     
+									</h4>
+							    </ul>	
+						<p> <br> </p>		
+		</div> </div> </div> </div>  <!-- fim da primeira coluna -->	
+	</body>				
+
+ 
+    
+    <body>
+         <div class="section" style="background: #FFEBCD; margin:0; padding:8px; border:1px solid #aaa">	<!-- #FFEBCD cor blanchedalmond -->			  
+              <div class="container"> 
+		  	 	   <div class="row">			    		
+                        <div class="col-md-10"> <!-- inÃ­cio da segunda coluna -->		
+					         <p></p>
+						     <h3><b><u>QUER ME CONHECER? CONVERSAR COMIGO ONLINE?</b></u></h3>
+						     <p><br></p>
+							 <h4 align="justify">
+							    <ul>
+                                     <li>Twitter: <a href="http://twitter.com/amgauna">@amgauna</a> |  
+									         <a href="http://twitter.com/AnaGaunaTech">@AnaGaunaTech</a> </li> <p>
+								     <li>Gravatar: <a href="http://pt.gravatar.com/amgauna">http://pt.gravatar.com/amgauna</a> </li> <p>
+								     <li>WebSite:  <a href="http://www.amgauna.eti.br">http://www.amgauna.eti.br</a> | 
+									               <a href="http://www.anagauna.eti.br">http://www.anagauna.eti.br</a> </li> <p>
+									 <li>Blog Wordpress:  <a href="http://anagauna.wordpress.com">http://anagauna.wordpress.com</a> </li> <p>
+									 <li>Blog Blogger:    <a href="http://anagaunatech.blogspot.com.br">http://anagaunatech.blogspot.com.br </a> </li> <p>
+									 <li>Blog MulherTech:  <a href="http://mulhertech.blogspot.com.br">http://mulhertech.blogspot.com.br </a> | 
+									                      <a href="http://www.mulhertech.com.br">http://www.mulhertech.com.br </a> </li>  <p>									
+								     <li>Linkedin: <a href="https://www.linkedin.com/in/amgauna/">https://www.linkedin.com/in/amgauna/</a> </li> <p>
+									 <li>Google+: <a href="https://plus.google.com/+AnaGaunaTech">https://plus.google.com/+AnaGaunaTech </a> </li> <p>
+									 <li>GitHUB: @amgauna  <a href="https://github.com/amgauna">https://github.com/amgauna</a> </li> <p>
+									 <li>Microsoft TechNet: <a href="https://social.technet.microsoft.com/profile/ana%20gauna/">
+									                                https://social.technet.microsoft.com/profile/ana%20gauna/</a> </li> <p>
+							         <li>Microsoft MSDN: <a href="https://social.msdn.microsoft.com/profile/ana%20gauna/">
+									                             https://social.msdn.microsoft.com/profile/ana%20gauna/</a> </li> <p>
+									 <li>Curriculo Lattes (CNPq): <a href="http://lattes.cnpq.br/6513217293754942">http://lattes.cnpq.br/6513217293754942 </a> </li> <p>
+								 </ul>
+							 </h4>
+								 <p> <br> </p>
+							<p><br></p>
+                            <h3><b>CURRICULUM VITAE de </b>
+                                   <a href="http://www.amgauna.eti.br/img/CV2017-ANAGAUNA-RESUMIDO.pdf" target="_blank">
+		                           Ana Mercedes Gauna <br>Download [Curriculo 2017 Resumido em 2 folhas] (PDF) </a> </h3>
+                            <p><br></p>
+		 </div> </div> </div> </div> <!-- fim da segunda coluna -->
+    </body>
+
+
+
+        <!-- desligando 3 colunas
+        <div class="section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4"></div>
+                </div>
+            </div>
+        </div> -->
+
+
+
+<?php require('rodape.php'); ?>
---
+++ rodape.php
@@ -0,0 +1,68 @@
+
+    <!-- inicio do rodape / Bootstrap / PHP  -->
+
+<footer>
+  <div class="section" style="background:aliceblue; margin:0px; padding:0px; border:1px solid #aaa"> 
+       <div class="container">
+            <div class="row">
+                 <div class="col-md-6 text-info text-left">
+                      <h6> <span style="color:black;"> <b> 1998-2017 </b> <b>&copy;</b> <b>Desenvolvido por:</b> 
+                           <b style="color:darkviolet;"> ANA MERCEDES GAUNA </b>
+                           <br>
+                           <span style="color:black;"> <b> CRA-RJ:  </b> <b style="color:darkviolet;"> 03-03161 </b> </span> | 
+                           <span style="color:black;"> <b> ABRAWEB: </b> <b style="color:darkviolet;"> 66132    </b> </span> |
+                           <span style="color:black;"> <b> ABRACEM: </b> <b style="color:darkviolet;"> Q27795   </b> </span> 
+                           <p> <p>
+                           <span style="color:darkblue;"> <b> Twitter: <b style="color:darkred;"> @amgauna </b> | 
+                           <span style="color:darkred;">@AnaGaunaTech | 
+                           <span style="color:darkblue;"> <b> GitHUB: </b> <b style="color:darkred;"> @amgauna </b> </span> 
+                           <br>
+                           <span style="color:darkblue;"> <b> Telefone/Fax: </b> <b style="color:darkviolet;"> +55 (21) 2538-0399  </b> </span> 
+                           <span style="color:darkblue;"> <b> | WhatsApp:   </b> <b style="color:darkviolet;"> +55 (21) 97937-1230 </b> </span>   
+                           <br>
+                           <span style="color:darkblue;"> <b> Email:   </b> <a href="mailto:#" style="color:darkviolet;"><b>amgauna@gmail.com</b></a> </span> 
+                           <span style="color:darkblue;"> <b> | Skype: </b> <a href="mailto:#" style="color:darkviolet;"><b>amgauna@hotmail.com</b></a> </span>
+                           <p>
+                           </h6> 
+                           <p> <br> </p>
+                     </div>
+                				 
+                      <div class="col-md-6 text-info text-right"> <p></p>
+                           <div class="row">
+                                <div class="col-md-12 hidden-lg hidden-md hidden-sm">
+                                     <a href="#"><i class="fa fa-2x fa-fw fa-twitter text-inverse"></i></a>
+                                     <a href="#"><i class="fa fa-2x fa-fw fa-twitter text-inverse"></i></a>
+                                     <a href="#"><i class="fa fa-2x fa-fw fa-linkedin text-inverse"></i></a>
+                                     <a href="#"><i class="fa fa-2x fa-fw fa-github text-inverse"></i></a>
+                                     <a href="#"><i class="fa fa-2x fa-fw fa-google text-inverse"></i></a>
+                                     <a href="#"><i class="fa fa-2x fa-fw fa-wordpress text-inverse"></i></a>
+                                     <a href="#"><i class="fa fa-2x fa-fw fa-medium text-inverse"></i></a>
+                                     <a href="#"><i class="fa fa-2x fa-fw fa-facebook text-inverse"></i></a>
+                                     <a href="#"><i class="fa fa-2x fa-fw fa-pinterest text-inverse"></i></a>
+                                     <a href="#"><i class="fa fa-2x fa-fw fa-flickr text-inverse"></i></a>
+                                     <a href="#"><i class="fa fa-2x fa-fw fa-instagram text-inverse"></i></a>
+                                     <a href="#"><i class="fa fa-2x fa-fw fa-tumbl text-inverse"></i></a>
+                                </div> 
+                           </div>
+                           <div class="row">
+                                <div class="col-md-12 hidden-xs text-right">
+                                     <a href="https://twitter.com/anagaunatech"><i class="fa fa-2x fa-fw fa-twitter text-inverse"></i></a>
+                                     <a href="https://twitter.com/amgauna"><i class="fa fa-2x fa-fw fa-twitter text-inverse"></i></a>
+                                     <a href="https://br.linkedin.com/in/amgauna"><i class="fa fa-2x fa-fw fa-linkedin text-inverse"></i></a>
+                                     <a href="https://github.com/amgauna"><i class="fa fa-2x fa-fw fa-github text-inverse"></i></a>
+                                     <a href="https://plus.google.com/+AnaGaunaTech"><i class="fa fa-2x fa-fw fa-google text-inverse"></i></a>
+                                     <a href="https://anagauna.wordpress.com/"><i class="fa fa-2x fa-fw fa-wordpress text-inverse"></i></a>
+                                     <a href="https://medium.com/@anagauna"><i class="fa fa-2x fa-fw fa-medium text-inverse"></i></a>
+                                     <a href="https://www.facebook.com/amgauna"><i class="fa fa-2x fa-fw fa-facebook text-inverse"></i></a>
+                                     <a href="https://www.pinterest.com/amgauna/"><i class="fa fa-2x fa-fw fa-pinterest text-inverse"></i></a>
+                                     <a href="https://flickr.com/amgauna"><i class="fa fa-2x fa-fw fa-flickr text-inverse"></i></a>
+                                     <a href="https://instagram.com/amgauna"><i class="fa fa-2x fa-fw fa-instagram text-inverse"></i></a>
+                                     <a href="https://tumblr.com/amgauna"><i class="fa fa-2x fa-fw fa-tumblr text-inverse"></i></a>
+                                     <p> <br> </p>
+                               </div>
+                         </div> 
+          </div> </div> </div>
+    </footer>
+    <!-- fim do rodape -->
+
+</html>
