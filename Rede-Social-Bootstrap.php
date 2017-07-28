974a0bf7a48c8a419758a001a2126c675cbccdff
Ana Mercedes Gauna <amgauna@gmail.com>
2017-07-28 11:42:54 -0300

    Create Rede-Social-Bootstrap.php

---
+++ Rede-Social-Bootstrap.php
@@ -0,0 +1,25 @@
+<body>
+
+<!--  Rede Social / Bootstrap -->
+<!--  Substitui o # pela URL da rede social que utiliza + seu nome-usuario -->
+
+<div class="col-md-8 text-info text-left">
+     <div class="row">
+          <div class="col-md-8 hidden-lg hidden-md hidden-sm text-left">
+               <a href="#"><i class="fa fa-2x fa-fw fa-twitter text-inverse"></i></a>
+               <a href="#"><i class="fa fa-2x fa-fw fa-twitter text-inverse"></i></a>
+               <a href="#"><i class="fa fa-2x fa-fw fa-linkedin text-inverse"></i></a>
+               <a href="#"><i class="fa fa-2x fa-fw fa-github text-inverse"></i></a>
+               <a href="#"><i class="fa fa-2x fa-fw fa-google text-inverse"></i></a>
+               <a href="#"><i class="fa fa-2x fa-fw fa-wordpress text-inverse"></i></a>
+               <a href="#"><i class="fa fa-2x fa-fw fa-medium text-inverse"></i></a>
+               <a href="#"><i class="fa fa-2x fa-fw fa-facebook text-inverse"></i></a>
+               <a href="#"><i class="fa fa-2x fa-fw fa-pinterest text-inverse"></i></a>
+               <a href="#"><i class="fa fa-2x fa-fw fa-flickr text-inverse"></i></a>
+               <a href="#"><i class="fa fa-2x fa-fw fa-instagram text-inverse"></i></a>
+               <a href="#"><i class="fa fa-2x fa-fw fa-tumbl text-inverse"></i></a>
+          </div> 
+     </div>
+<div 
+                           
+</body>                           
